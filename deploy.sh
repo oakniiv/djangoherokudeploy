e224cb117f4bca7f5231d18c26d948d8aef8506a
Vinko Rakić <vinkorakic@oakniiv.com>
2023-12-01 18:40:20 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-# Function to deploy to Heroku
 deploy_to_heroku() {
-    echo "Running Django migrations..."
-    python manage.py makemigrations
-    python manage.py migrate
-
     echo "Deploying to Heroku app: APPNAME..."
     git push heroku main
-}
 
-# Add all changes to git
+    echo "Running migrations on Heroku..."
+    heroku run python manage.py makemigrations --app APPNAME
+    heroku run python manage.py migrate --app APPNAME
+}
 git add .
 
-# Check if there are any changes
 if git diff-index --quiet HEAD --; then
     echo "No changes"
 else
+    # local
+    python manage.py makemigrations
+    python manage.py migrate
+
     # Commit changes
     git commit -m "automated runserver commit" #TODO, maybe ask for commit name?
+    git push
 
     # Ask for deployment
     read -p "Do you want to deploy to Heroku? (y/n): " deploy_answer
@@ -29,9 +30,7 @@ else
     fi
 fi
 
-# run local
-python manage.py makemigrations
-python manage.py migrate
+# local
 python manage.py makemessages -l de # German translations
 python manage.py compilemessages
 python manage.py runserver 8000
